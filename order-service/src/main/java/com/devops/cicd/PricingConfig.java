24bf65570f8895f81cd09f9b8ef3084e96bdd2df
Alice DARONDEAU <alice.darondeau2025@campus-eni.fr>
2026-01-19 14:31:11 +0100

    add CI

--- order-service/src/main/java/com/devops/cicd/PricingConfig.java
+++ order-service/src/main/java/com/devops/cicd/PricingConfig.java
@@ -1,7 +1,9 @@
 package com.devops.cicd;
 
 public class PricingConfig {
+    // vatRate : taux TVA
     private final double vatRate;
+    // seuil de frais de livraison offert
     private final double freeShippingThreshold;
 
     public PricingConfig(double vatRate, double freeShippingThreshold) {
